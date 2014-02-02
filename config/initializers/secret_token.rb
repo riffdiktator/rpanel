874ed4775af27c2986a947b7f0d98accb24f1c2b
Tomasz Lipiński <developer@riffdiktator.com>
2014-02-02 20:22:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rpanel::Application.config.secret_key_base = '2cd49ce58fa20334a4caf99db5448be2585a83d9d30f6a86f75feb96f20ff99f4077b26c60f7894239334076a4e6755dde6c6a67a7abe5ae21bd4bd556a0ad6b'
