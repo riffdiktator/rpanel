56b317cce09e7353e957d97db3a6128b1a0076fc
Tomasz Lipiński <developer@riffdiktator.com>
2014-02-09 23:05:27 +0100

    Nieskonczona edycja podstrony

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,6 +2,7 @@ class Admin::PagesController < ApplicationController
 	before_action :authenticate_user!
 	
   def index
+  	@pages = Page.all
   end
 
   def new
@@ -15,6 +16,21 @@ class Admin::PagesController < ApplicationController
   end
   
   def edit
+    @Page_title = Page.find(params[:id]).title
+    @Page_text = Page.find(params[:id]).text
+    @Page_in_menu = Page.find(params[:id]).in_menu
+    @Page_created = Page.find(params[:id]).created_at
+    @Page_updated = Page.find(params[:id]).updated_at
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    @page.update(page_params)
+    if page_params[:save_and_leave]==1
+      redirect_to admin_pages_url
+    else
+      redirect_to edit_admin_page_url(@page)
+    end
   end
 
   private
